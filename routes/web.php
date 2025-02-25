946486f77c6dcce06cebde5386ab25ba3f393a57
Nikhilsinh1818 <nikhilsinh1818@gmail.com>
2025-02-25 19:55:33 +0530

    companies route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JobsController;
+use App\Http\Controllers\CompaniesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,3 +31,4 @@ Route::get('/register', function () {
     return view('auth.register'); // Create a register page later
 });
 Route::get('/jobs',[JobsController::class, 'index']);
+Route::get('/companies',[CompaniesController::class,'index']);

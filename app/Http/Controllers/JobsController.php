245a7534588132afa6fd1216a19c15bf3d658d75
mohit5552 <chikupatel574@gmail.com>
2025-02-25 18:08:13 +0530

    Add files via upload

---
+++ app/Http/Controllers/JobsController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class JobsController extends Controller
+{
+    // Display a listing of jobs
+    public function index()
+    {
+        // Example: return a view with jobs
+        return view('jobs.index');
+    }
+}
+
+//     // Show a specific job
+//     public function show($id)
+//     {
+//         // Example: find and return a specific job
+//         $job = Job::findOrFail($id);
+//         return view('jobs.show', compact('job'));
+//     }
+
+//     // Create a new job
+//     public function create()
+//     {
+//         return view('jobs.create');
+//     }
+
+//     // Store a newly created job
+//     public function store(Request $request)
+//     {
+//         $validated = $request->validate([
+//             'title' => 'required|string|max:255',
+//             'description' => 'required|string',
+//         ]);
+
+//         $job = Job::create($validated);
+
+//         return redirect()->route('jobs.index');
+//     }
+
+//     // Edit an existing job
+//     public function edit($id)
+//     {
+//         $job = Job::findOrFail($id);
+//         return view('jobs.edit', compact('job'));
+//     }
+
+//     // Update an existing job
+//     public function update(Request $request, $id)
+//     {
+//         $validated = $request->validate([
+//             'title' => 'required|string|max:255',
+//             'description' => 'required|string',
+//         ]);
+
+//         $job = Job::findOrFail($id);
+//         $job->update($validated);
+
+//         return redirect()->route('jobs.index');
+//     }
+
+//     // Delete a job
+//     public function destroy($id)
+//     {
+//         $job = Job::findOrFail($id);
+//         $job->delete();
+
+//         return redirect()->route('jobs.index');
+//     }
+// }
+

dc1cbc822eab76bd74caa367a8dc29aaf64d5c61
Nikhilsinh1818 <nikhilsinh1818@gmail.com>
2025-02-26 17:29:27 +0530

    Companies and Candidate CSS

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -53,7 +53,46 @@
                         </div>
                     </li>
 
-                    <li class="dropdown">
+                    <li class="dropdown has-mega-menu" id="has-mega-menu">
+                        <span><a href="{{ url('/companies') }}">Companies</a></span>
+                        <div class="mega-menu">
+                            <div class="mega-menu-bar row">
+                                <div class="column col-lg-3 col-md-3 col-sm-12">
+                                    <h3>Explore categories</h3>
+                                    <ul>
+                                        <li><a href="{{ url('compines-list-v1.html') }}">Unicorn</a></li>
+                                        <li><a href="{{ url('compines-list-v1.html') }}">MNC</a></li>
+                                        <li><a href="{{ url('compines-list-v1.html') }}">Startup</a></li>
+                                        <li><a href="{{ url('compines-list-v1.html') }}">Product Based</a></li>
+                                        <li><a href="{{ url('compines-list-v1.html') }}">Internet</a></li>
+                                    </ul>
+                                </div>
+
+                                <div class="column col-lg-3 col-md-3 col-sm-12">
+                                    <h3>Explore collections</h3>
+                                    <ul>
+                                        <li><a href="{{ url('compines-single-v1.html') }}">Top companies</a></li>
+                                        <li><a href="{{ url('compines-single-v1.html') }}">IT companies</a></li>
+                                        <li><a href="{{ url('compines-single-v1.html') }}">Fintech companies</a></li>
+                                        <li><a href="{{ url('compines-single-v1.html') }}">Sponsored companies</a></li>
+                                        <li><a href="{{ url('compines-single-v1.html') }}">Featured companies</a></li>
+                                    </ul>
+                                </div>
+
+                                <div class="column col-lg-3 col-md-3 col-sm-12">
+                                    <h3>Research companies</h3>
+                                    <ul>
+                                        <li><a href="{{ url('compines-single-v1.html') }}">Interview questions</a></li>
+                                        <li><a href="{{ url('compines-single-v1.html') }}">Company salaries</a></li>
+                                        <li><a href="{{ url('compines-single-v1.html') }}">Company reviews</a></li>
+                                        <li><a href="{{ url('compines-single-v1.html') }}">Salary calculator</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+
+                    <!-- <li class="dropdown">
                         <span><a href="{{ url('/companies') }}">Companies</a></span>
                         <ul>
                             <li class="dropdown">
@@ -69,8 +108,35 @@
                                 </ul>
                             </li>
                         </ul>
+                    </li> -->
+
+                    <li class="dropdown has-mega-menu" id="has-mega-menu">
+                        <span><a href="">Candidates</a></span>
+                        <div class="mega-menu">
+                            <div class="mega-menu-bar row">
+                                <div class="column col-lg-3 col-md-3 col-sm-12">
+                                    <h3>Candidates List</h3>
+                                    <ul>
+                                        <li><a href="{{ url('candidates-list-v1.html') }}">Candidates LIst v1</a></li>
+                                    </ul>
+                                </div>
+
+                                <div class="column col-lg-3 col-md-3 col-sm-12">
+                                    <h3>Candidates Single</h3>
+                                    <ul>
+                                        <li><a href="{{ url('candidates-single-v1.html') }}">Candidates Single v1</a></li>
+                                    </ul>
+                                </div>
+                                
+                                <div class="column col-lg-3 col-md-3 col-sm-12">
+                                    <h3><a href="{{ url('candidate-dashboard.html') }}">Candidates Dashboard</a></h3>
+                                </div>
+                            </div>
+                        </div>
                     </li>
-                    <li class="dropdown">
+
+
+                    <!-- <li class="dropdown">
                         <span>Candidates</span>
                         <ul>
                             <li class="dropdown">
@@ -87,7 +153,10 @@
                             </li>
                             <li><a href="{{ url('candidate-dashboard.html') }}">Candidates Dashboard</a></li>
                         </ul>
-                    </li>
+                    </li> -->
+
+
+
                     <li class="dropdown">
                         <span>Blog</span>
                         <ul>

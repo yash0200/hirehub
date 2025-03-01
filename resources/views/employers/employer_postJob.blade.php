32f38685cbdb66422233b0316a5ab534343b497a
Yash jikadara <133654275+yash0200@users.noreply.github.com>
2025-03-01 12:15:00 +0530

    postjob design

--- resources/views/employers/employer_postJob.blade.php
+++ resources/views/employers/employer_postJob.blade.php
@@ -8,65 +8,37 @@
 <section class="user-dashboard">
     <div class="dashboard-outer">
         <div class="upper-title-box">
-            <h3>Post a New Job!</h3>
-            <div class="text">Ready to jump back in?</div>
+            <h3 class="dark-color">Post a New Job!</h3>
+            <p class="text-color">Ready to jump back in?</p>
         </div>
 
         <div class="row">
             <div class="col-lg-12">
-                <!-- Ls widget -->
                 <div class="ls-widget">
                     <div class="tabs-box">
                         <div class="widget-title">
-                            <h4>Post Job</h4>
+                            <h4 class="dark-color">Post Job</h4>
                         </div>
 
                         <div class="widget-content">
-
                             <div class="post-job-steps">
-                                <div class="step">
-                                    <span class="icon flaticon-briefcase"></span>
-                                    <h5>Job Detail</h5>
-                                </div>
-
-                                <div class="step">
-                                    <span class="icon flaticon-money"></span>
-                                    <h5>Package & Payments</h5>
-                                </div>
-
-                                <div class="step">
-                                    <span class="icon flaticon-checked"></span>
-                                    <h5>Confirmation</h5>
-                                </div>
+                                <div class="step"><span class="icon flaticon-briefcase"></span><h5>Job Detail</h5></div>
+                                <div class="step"><span class="icon flaticon-money"></span><h5>Package & Payments</h5></div>
+                                <div class="step"><span class="icon flaticon-checked"></span><h5>Confirmation</h5></div>
                             </div>
 
                             <form class="default-form">
                                 <div class="row">
-                                    <!-- Input -->
-                                    <div class="form-group col-lg-12 col-md-12">
+                                    <div class="form-group col-lg-12">
                                         <label>Job Title</label>
-                                        <input type="text" name="name" placeholder="Title">
+                                        <input type="text" name="name" class="form-control" placeholder="Title">
                                     </div>
 
-                                    <!-- About Company -->
-                                    <div class="form-group col-lg-12 col-md-12">
+                                    <div class="form-group col-lg-12">
                                         <label>Job Description</label>
-                                        <textarea
-                                            placeholder="Spent several years working on sheep on Wall Street. Had moderate success investing in Yugo's on Wall Street. Managed a small team buying and selling Pogo sticks for farmers. Spent several years licensing licorice in West Palm Beach, FL. Developed several new methods for working it banjos in the aftermarket. Spent a weekend importing banjos in West Palm Beach, FL.In this position, the Software Engineer collaborates with Evention's Development team to continuously enhance our current software solutions as well as create new solutions to eliminate the back-office operations and management challenges present"></textarea>
+                                        <textarea class="form-control" placeholder="Enter job details..."></textarea>
                                     </div>
-
-
-                                    <!-- Input -->
-                                    <div class="form-group col-lg-6 col-md-12">
-                                        <label>Email Address</label>
-                                        <input type="text" name="name" placeholder="">
-                                    </div>
-
-                                    <!-- Input -->
-                                    <div class="form-group col-lg-6 col-md-12">
-                                        <label>Username</label>
-                                        <input type="text" name="name" placeholder="">
-                                    </div>
+                                
 
                                     <!-- Search Select -->
                                     <div class="form-group col-lg-6 col-md-12">
@@ -82,7 +54,7 @@
                                     </div>
 
                                     <div class="form-group col-lg-6 col-md-12">
-                                        <label>Job Type</label>
+                                        <label>Job Category</label>
                                         <select class="chosen-select">
                                             <option>Select</option>
                                             <option>Banking</option>
@@ -94,28 +66,15 @@
                                     </div>
 
                                     <!-- Input -->
-                                    <div class="form-group col-lg-6 col-md-12">
-                                        <label>Offered Salary</label>
-                                        <select class="chosen-select">
+                                    <div class="form-group col-lg-6">
+                                        <label>Offered Salary (INR Per Annum)</label>
+                                        <select class="chosen-select form-control">
                                             <option>Select</option>
-                                            <option>$1500</option>
-                                            <option>$2000</option>
-                                            <option>$2500</option>
-                                            <option>$3500</option>
-                                            <option>$4500</option>
-                                            <option>$5000</option>
-                                        </select>
-                                    </div>
-
-                                    <div class="form-group col-lg-6 col-md-12">
-                                        <label>Career Level</label>
-                                        <select class="chosen-select">
-                                            <option>Select</option>
-                                            <option>Banking</option>
-                                            <option>Digital & Creative</option>
-                                            <option>Retail</option>
-                                            <option>Human Resources</option>
-                                            <option>Management</option>
+                                            <option>₹3,00,000</option>
+                                            <option>₹5,00,000</option>
+                                            <option>₹7,00,000</option>
+                                            <option>₹10,00,000</option>
+                                            <option>₹15,00,000</option>
                                         </select>
                                     </div>
 
@@ -171,42 +130,43 @@
                                         <input type="text" name="name" placeholder="06.04.2020">
                                     </div>
 
-                                    <!-- Input -->
-                                    <div class="form-group col-lg-6 col-md-12">
+                                    <div class="form-group col-lg-6">
                                         <label>Country</label>
-                                        <select class="chosen-select">
-                                            <option>Australia</option>
-                                            <option>Pakistan</option>
-                                            <option>Chaina</option>
-                                            <option>Japan</option>
-                                            <option>India</option>
-                                        </select>
+                                        <input type="text" name="country" class="form-control" value="India" readonly>
                                     </div>
 
-                                    <!-- Input -->
-                                    <div class="form-group col-lg-6 col-md-12">
+                                    <div class="form-group col-lg-6">
+                                        <label>State</label>
+                                        <input type="text" name="state" class="form-control" placeholder="Enter state">
+                                    </div>
+
+                                    <div class="form-group col-lg-6">
                                         <label>City</label>
-                                        <select class="chosen-select">
-                                            <option>Melbourne</option>
-                                            <option>Pakistan</option>
-                                            <option>Chaina</option>
-                                            <option>Japan</option>
-                                            <option>India</option>
-                                        </select>
+                                        <input type="text" name="city" class="form-control" placeholder="Enter city">
                                     </div>
 
-                                    <!-- Input -->
-                                    <div class="form-group col-lg-12 col-md-12">
+                                    <div class="form-group col-lg-6">
+                                        <label>Postcode</label>
+                                        <input type="text" name="postcode" class="form-control" placeholder="Enter postcode">
+                                    </div>
+
+                                    
+
+                                    <div class="form-group col-lg-12">
                                         <label>Complete Address</label>
-                                        <input type="text" name="name"
-                                            placeholder="329 Queensberry Street, North Melbourne VIC 3051, Australia.">
+                                        <input type="text" name="address" class="form-control" placeholder="Enter address">
                                     </div>
 
+<<<<<<< Updated upstream
                                    
 
                                     <!-- Input -->
                                     <div class="form-group col-lg-12 col-md-12 text-right">
                                         <button class="theme-btn btn-style-one">Next</button>
+=======
+                                    <div class="form-group col-lg-12 text-right">
+                                        <button class="theme-btn btn-style-one" type="submit">Submit</button>
+>>>>>>> Stashed changes
                                     </div>
                                 </div>
                             </form>
@@ -214,15 +174,12 @@
                     </div>
                 </div>
             </div>
-
-
         </div>
     </div>
 </section>
-<!-- End Dashboard -->
 
 <!-- Copyright -->
-<div class="copyright-text">
-    <p>© 2025 HireHub. All Right Reserved.</p>
+<div class="copyright-text text-center">
+    <p>&copy; 2025 HireHub. All Rights Reserved.</p>
 </div>
 @endsection

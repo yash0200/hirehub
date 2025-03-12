d53a9dd0560328d2191cc6c953e01b966b7a827b
mohit5552 <chikupatel574@gmail.com>
2025-03-12 19:34:13 +0530

    citydynamic

--- app/Providers/UserTypeServiceProvider.php
+++ app/Providers/UserTypeServiceProvider.php
@@ -16,9 +16,17 @@ class UserTypeServiceProvider extends ServiceProvider
     {
         View::composer('*', function ($view) {
             $userType = Auth::check() ? Auth::user()->user_type : null;
-            $categories = JobCategory::all(); // Fetch categories
+            $categories = JobCategory::where('status', 'active')->withCount('jobs')->get();
             $locations = ['Delhi', 'Mumbai', 'Bangalore', 'Hyderabad', 'Chennai', 'Pune'];
-            $view->with(compact('userType', 'categories','locations'));
+            $cities = [
+                ['name' => 'Delhi', 'jobs' => 96, 'image' => 'home22-city-1.png'],
+                ['name' => 'Mumbai', 'jobs' => 96, 'image' => 'home22-city-2.png'],
+                ['name' => 'Bangalore', 'jobs' => 96, 'image' => 'home22-city-4.png'],
+                ['name' => 'Hyderabad', 'jobs' => 96, 'image' => 'home22-city-5.png'],
+                ['name' => 'Chennai', 'jobs' => 96, 'image' => 'home22-city-6.png'],
+                ['name' => 'Pune', 'jobs' => 96, 'image' => 'home22-city-7.png'],
+            ];
+            $view->with(compact('userType', 'categories','locations','cities'));
            
         });
     }

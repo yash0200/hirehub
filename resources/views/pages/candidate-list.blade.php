405ca6f3018689b4f2736471fed36fd145b0c1dc
Nikhilsinh1818 <nikhilsinh1818@gmail.com>
2025-03-19 19:33:47 +0530

    Candidate List

--- resources/views/pages/candidate-list.blade.php
+++ resources/views/pages/candidate-list.blade.php
@@ -15,13 +15,12 @@
           <div class="filters-column col-lg-4 col-md-12 col-sm-12">
             <div class="inner-column pd-right">
               <div class="filters-outer">
-                <button type="button" class="theme-btn close-filters">X</button>
-
+                <form action="{{ route('candidates.list') }}" method="GET">
                 <!-- Filter Block -->
                 <div class="filter-block">
                   <h4>Search by Keywords</h4>
                   <div class="form-group">
-                    <input type="text" name="listing-search" placeholder="Job title, keywords, or company">
+                    <input type="text" name="listing-search" placeholder="keywords">
                     <span class="icon flaticon-search-3"></span>
                   </div>
                 </div>
@@ -33,29 +32,9 @@
                     <input type="text" name="listing-search" placeholder="City or postcode">
                     <span class="icon flaticon-map-locator"></span>
                   </div>
-                  <p>Radius around selected destination</p>
-                  <div class="range-slider-one">
-                    <div class="area-range-slider"></div>
-                    <div class="input-outer">
-                      <div class="amount-outer"><span class="area-amount"></span>km</div>
-                    </div>
-                  </div>
                 </div>
 
-                <!-- Filter Block -->
-                <div class="filter-block">
-                  <h4>Category</h4>
-                  <div class="form-group">
-                    <select class="chosen-select">
-                      <option>Choose a category</option>
-                      <option>Residential</option>
-                      <option>Commercial</option>
-                      <option>Industrial</option>
-                      <option>Apartments</option>
-                    </select>
-                    <span class="icon flaticon-briefcase"></span>
-                  </div>
-                </div>
+
 
                 <!-- Filter Block -->
                 <div class="filter-block">
@@ -64,42 +43,11 @@
                     <select class="chosen-select">
                       <option>Male</option>
                       <option>Female</option>
-                      <option>Others</option>
                     </select>
                     <span class="icon flaticon-man"></span>
                   </div>
                 </div>
 
-                <!-- Checkboxes Ouer -->
-                <div class="checkbox-outer">
-                  <h4>Date Posted</h4>
-                  <ul class="checkboxes square">
-                    <li>
-                      <input id="check-f" type="checkbox" name="check">
-                      <label for="check-f">All</label>
-                    </li>
-                    <li>
-                      <input id="check-g" type="checkbox" name="check">
-                      <label for="check-g">Last Hour</label>
-                    </li>
-                    <li>
-                      <input id="check-h" type="checkbox" name="check">
-                      <label for="check-h">Last 24 Hours</label>
-                    </li>
-                    <li>
-                      <input id="check-i" type="checkbox" name="check">
-                      <label for="check-i">Last 7 Days</label>
-                    </li>
-                    <li>
-                      <input id="check-j" type="checkbox" name="check">
-                      <label for="check-j">Last 14 Days</label>
-                    </li>
-                    <li>
-                      <input id="check-k" type="checkbox" name="check">
-                      <label for="check-k">Last 30 Days</label>
-                    </li>
-                  </ul>
-                </div>
 
 
                 <!-- Checkboxes Ouer -->
@@ -126,41 +74,13 @@
                       <input id="check-p" type="checkbox" name="check">
                       <label for="check-p">20-25 Years</label>
                     </li>
-                    <li>
-                      <button class="view-more"><span class="icon flaticon-plus"></span> View More</button>
-                    </li>
                   </ul>
                 </div>
-
-                <!-- Checkboxes Ouer -->
-                <div class="checkbox-outer">
-                  <h4>Education Levels</h4>
-                  <ul class="checkboxes square">
-                    <li>
-                      <input id="check-a" type="checkbox" name="check">
-                      <label for="check-a">Certificate</label>
-                    </li>
-                    <li>
-                      <input id="check-b" type="checkbox" name="check">
-                      <label for="check-b">Diploma</label>
-                    </li>
-                    <li>
-                      <input id="check-c" type="checkbox" name="check">
-                      <label for="check-c">Associate Degree</label>
-                    </li>
-                    <li>
-                      <input id="check-d" type="checkbox" name="check">
-                      <label for="check-d">Bachelor Degree</label>
-                    </li>
-                    <li>
-                      <input id="check-e" type="checkbox" name="check">
-                      <label for="check-e">Master’s Degree</label>
-                    </li>
-                    <li>
-                      <button class="view-more"><span class="icon flaticon-plus"></span> View More</button>
-                    </li>
-                  </ul>
-                </div>
+                <div class="filter-buttons">
+                <button type="submit" class="btn btn-primary">Apply</button>
+                <a href="{{ route('candidates.list') }}" class="btn btn-secondary">Reset</a>
+              </div>
+</form>
               </div>
             </div>
           </div>
@@ -207,7 +127,7 @@
                 <img src="{{ asset('/storage/profile_photos/' . $candidate->profile_photo) }}">
                 </figure>
                 <h4 class="name">
-                    <a href="{{ route('candidate.profile', $candidate->id) }}">{{ $candidate->full_name }}</a>
+                    <a href="{{ route('employer.applicant.profile', $candidate->id) }}">{{ $candidate->full_name }}</a>
                 </h4>
                 <ul class="candidate-info">
                     <li class="designation">{{ $candidate->designation }}</li>
@@ -217,7 +137,7 @@
                     </li>
                     <li>
                         <span class="icon flaticon-money"></span> 
-                        ${{ $candidate->hourly_rate }} / hour
+                        {{ $candidate->resume->current_salary??'N/A' }} 
                     </li>
                 </ul>
                 <ul class="post-tags">
@@ -230,7 +150,7 @@
             </div>
             <div class="btn-box">
                 <button class="bookmark-btn"><span class="flaticon-bookmark"></span></button>
-                <a href="{{ route('candidate.profile', $candidate->id) }}" class="theme-btn btn-style-three">
+                <a href="{{ route('employer.applicant.profile', $candidate->id) }}" class="theme-btn btn-style-three">
                     <span class="btn-title">View Profile</span>
                 </a>
             </div>

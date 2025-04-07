3a5bef5889a8c37a42bfa57018c292f021691adb
Nikhilsinh1818 <nikhilsinh1818@gmail.com>
2025-04-07 09:53:23 +0530

    Error Solve

--- resources/views/employers/employer_profile.blade.php
+++ resources/views/employers/employer_profile.blade.php
@@ -22,6 +22,12 @@
 
                             <h4>My Profile</h4>
                         </div>
+                        @if (session('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+              {{ session('success') }}
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            @endif
 
                         <div class="widget-content">
                             <form class="default-form" action="{{ route('employer.company.profile.update') }}"
